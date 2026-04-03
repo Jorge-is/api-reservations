e6248132a8f7edbac4195907d4c49daed1952d99
Jorge Flores <jorgeafa.19@gmail.com>
2026-04-03 18:33:46 -0500

    ✨ feat(validation): agregar validadores personalizados para ciudades

--- src/main/java/com/edteam/reservations/validation/CityFormatConstraint.java
+++ src/main/java/com/edteam/reservations/validation/CityFormatConstraint.java
@@ -1,4 +1,18 @@
 package com.edteam.reservations.validation;
 
-public interface CityFormatConstraint {
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = CityFormatValidator.class)
+@Target({ElementType.METHOD, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CityFormatConstraint {
+    String message() default "Invalid format of the city.";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
 }
